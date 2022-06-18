f657d192e2aaf833208d36f3a8769e3393679ec3
ziduoyi <78286708+ziduoyi@users.noreply.github.com>
2022-06-17 19:52:07 -0500

    j

--- popup.js
+++ popup.js
@@ -12,8 +12,8 @@ window.onload = getNumber; // Runs the function on click
 
 
 // The data/time we want to countdown to
- var countDownDate = new Date("Jul 25, 2022 16:37:52").getTime();
-
+ //var countDownDate = new Date("Jul 25, 2022 16:37:52").getTime();
+var countDownDate = new Date().getTime()+15*60*1000;
 // Run myfunc every second
 var myfunc = setInterval(function() {
 
@@ -21,22 +21,16 @@ var now = new Date().getTime();
 var timeleft = countDownDate - now;
     
 // Calculating the days, hours, minutes and seconds left
-var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
-var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
 var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
     
 // Result is output to the specific element
-document.getElementById("days").innerHTML = days + "d "
-document.getElementById("hours").innerHTML = hours + "h " 
 document.getElementById("mins").innerHTML = minutes + "m " 
 document.getElementById("secs").innerHTML = seconds + "s " 
     
 // Display the message when countdown is over
 if (timeleft < 0) {
     clearInterval(myfunc);
-    document.getElementById("days").innerHTML = ""
-    document.getElementById("hours").innerHTML = "" 
     document.getElementById("mins").innerHTML = ""
     document.getElementById("secs").innerHTML = ""
     document.getElementById("end").innerHTML = "TIME UP!!";
