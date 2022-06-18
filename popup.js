26e6071ed0f509d1fc92a8b4c833a99a6baef61d
baris <berkaybildaci@gmail.com>
2022-06-17 23:19:03 -0500

    auihdfa

--- popup.js
+++ popup.js
@@ -1,3 +1,14 @@
+//Random number generator
+var countDownDate=0;
+document.addEventListener('DOMContentLoaded', function() {
+  var link = document.getElementById('timeButton');
+  link.addEventListener('click', function(){
+      countDownDate = new Date().getTime()+15*60*1000;
+      getTime(); 
+      
+  });
+  getTime(); 
+});
 
 function getNumber() {
   var minNumber = 0; // The minimum number you want
@@ -11,11 +22,7 @@ window.onload = getNumber; // Runs the function on click
 
 function getTime()
 {
-  
-
-// The data/time we want to countdown to
- //var countDownDate = new Date("Jul 25, 2022 16:37:52").getTime();
-
+ 
 // Run myfunc every second
 var myfunc = setInterval(function() {
 
@@ -26,23 +33,9 @@ var timeleft = countDownDate - now;
 var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
 var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
 
-//chrome.runtime.onInstalled.addListener(() => {
-//  chrome.storage.sync.set({minutes: minutes });
-  
-//});
-
-// chrome.storage.sync.set({ minutes });
-// chrome.storage.sync.set({ seconds }); 
-
-//chrome.stroage.sync.get(({ minutes }) => {
-//document.getElementById("mins").innerHTML = minutes;
-//});
-
 // Result is output to the specific element
-document.getElementById("mins").innerHTML = minutes + "m " 
-document.getElementById("secs").innerHTML = seconds  + "s " 
-  
-
+ document.getElementById("mins").innerHTML = minutes + "m " 
+ document.getElementById("secs").innerHTML = seconds  + "s " 
 
 // Display the message when countdown is over
 if (timeleft < 0) {
