3ceb5c3bb115c075b7d26f839b3f3c248690e34f
baris <berkaybildaci@gmail.com>
2022-06-17 19:45:43 -0500

    a

--- popup.js
+++ popup.js
@@ -21,7 +21,7 @@ var now = new Date().getTime();
 var timeleft = countDownDate - now;
     
 // Calculating the days, hours, minutes and seconds left
-var days = Math.floor(timeleft / (1000 * 60 * 60 * 24))+9999;
+var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
 var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
 var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
