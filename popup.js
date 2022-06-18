7a689121abc37e44d037dd54675aac608a0a62ce
baris <berkaybildaci@gmail.com>
2022-06-17 20:25:33 -0500

    asda

--- popup.js
+++ popup.js
@@ -1,4 +1,40 @@
-timeButton.addEventListener("click", async () => {
+//Random number generator
 
-  
-});
+function getNumber() {
+  var minNumber = 0; // The minimum number you want
+  var maxNumber = 100; // The maximum number you want
+  var randomnumber = Math.floor(Math.random() * (maxNumber + 1) + minNumber); // Generates random number
+  document.getElementById("myNumber").innerHTML = randomnumber; // Sets content of <div> to number
+  return false; // Returns false just to tidy everything up
+}
+
+window.onload = getNumber; // Runs the function on click
+
+
+// The data/time we want to countdown to
+ //var countDownDate = new Date("Jul 25, 2022 16:37:52").getTime();
+var countDownDate = new Date().getTime()+15*60*1000;
+// Run myfunc every second
+var myfunc = setInterval(function() {
+
+var now = new Date().getTime();
+var timeleft = countDownDate - now;
+    
+// Calculating the days, hours, minutes and seconds left
+var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+    
+// Result is output to the specific element
+document.getElementById("mins").innerHTML = minutes + "m " 
+document.getElementById("secs").innerHTML = seconds + "s " 
+    
+console.log("working");
+// Display the message when countdown is over
+if (timeleft < 0) {
+    clearInterval(myfunc);
+    document.getElementById("mins").innerHTML = ""
+    document.getElementById("secs").innerHTML = ""
+    document.getElementById("end").innerHTML = "TIME UP!!";
+}
+}, 1000);
+
